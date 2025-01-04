39fe01d25d879d7c8f6ca6565116ebabd2c095db
Jesse Hämmerle <jesse@haemmerle.at>
2025-01-04 17:07:02 +0100

    Anpassungen an der Build-Datei

--- build.py
+++ build.py
@@ -17,15 +17,19 @@ options = [
     main_script,                # Hauptprogramm
     '--onefile',                # Eine einzige ausführbare Datei erstellen
     '--noconfirm',              # Kein Bestätigungsdialog
-    '--name=MemberWorks_Beta_0_4_0', # Name der ausführbaren Datei
     f'--add-data={templates_dir}{os.pathsep}templates', # Vorlagen
     f'--add-data={static_dir}{os.pathsep}static',       # Statische Dateien
     f'--add-data={models_path}{os.pathsep}.',           # models.py
     f'--add-data={forms_path}{os.pathsep}.',            # forms.py
     f'--add-data={services_path}{os.pathsep}.',         # services.py
+    '--collect-all=pysqlite3', 
+    '--collect-all=MySQLdb',
+    '--collect-all=psycopg2',
     '--hidden-import=pysqlite2',                        # Hidden Import: pysqlite2
     '--hidden-import=MySQLdb',                          # Hidden Import: MySQLdb
     '--hidden-import=psycopg2',                         # Hidden Import: psycopg2
+    '--name=MemberWorks_Beta_0_4_2', # Name der ausführbaren Datei
+    '--log-level=DEBUG'
 ]
 
 # PyInstaller ausführen
